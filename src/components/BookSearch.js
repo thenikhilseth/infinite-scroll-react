997a6f62709f6dcfef26fcbd5d26724bec5f396b
Nikhil Seth <nikhilseth0078@gmail.com>
2024-05-08 08:18:08 -0400

    Abort controller completed..Moving to setState

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -1,8 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 
 function BookSearch(query, pageNumber) {
+  const controllerRef = useRef(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [books, setBooks] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
+    setError(false);
+    //If controller already exists on changeHandle
+    if (controllerRef.current) {
+      controllerRef.current.abort();
+    }
+    controllerRef.current = new AbortController();
+    const signal = controllerRef.current.signal;
+
     axios({
       method: "GET",
       url: "https://openlibrary.org/search.json",
@@ -10,16 +25,32 @@ function BookSearch(query, pageNumber) {
         q: query,
         page: pageNumber,
       },
+      signal,
     })
       .then((response) => {
         console.log(response.data);
+        setBooks((prevBooks) => {
+          return [
+            ...prevBooks,
+            ...response.data.docs.map((book) => {
+              return book.title;
+            }),
+          ];
+        });
+        setHasMore(response.data.docs.length > 0);
+        setLoading(false);
       })
       .catch((e) => {
-        console.log(e);
+        setError(true);
+        if (axios.isCancel(e)) {
+          //If the error is axios cancel error because of abort controller, don't display the error because we cancelled it intentionally
+          return;
+        }
+        console.log("Data Fetching Error");
       });
   }, [query, pageNumber]);
 
-  return null;
+  return { loading, error, books, hasMore };
 }
 
 export default BookSearch;
